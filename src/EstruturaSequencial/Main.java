dd7828bac9b78047c6dd34018205552055453951
AndreCabral1 <andre_kbral@hotmail.com>
2024-03-20 20:11:02 -0300

    exercicio estrutura condicional!

---
+++ src/EstruturaSequencial/Main.java
@@ -0,0 +1,40 @@
+package EstruturaSequencial;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+
+        double x = 10.35784;
+        System.out.println("Hello world!");
+        System.out.println("Bom dia!");
+        System.out.println(x);
+        System.out.println("----------------------------");
+        System.out.printf("%.2f", x);
+        System.out.println();
+        System.out.println("Resultado = " + x + " metros");
+        System.out.printf("Resultados = %.2f metros", x);
+        System.out.println();
+
+        Scanner entrada = new Scanner(System.in);
+
+        int y;
+        System.out.println("Digite um numero");
+        y = entrada.nextInt();
+        System.out.println("Voce digitou:" + y);
+
+        double z;
+        System.out.println("Digite um numero");
+        z = entrada.nextDouble();
+        System.out.printf("Você digitou: %.2f%n", z);
+
+        char caracter;
+        System.out.println("Digite caracter");
+        caracter = entrada.next().charAt(0);
+        System.out.println("Voce digitou:" + caracter);
+
+        entrada.close();
+    }
+}
---
+++ src/ExerciciosEstruturaCondicional/Main.java
@@ -0,0 +1,156 @@
+package ExerciciosEstruturaCondicional;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+
+        /*exercicio 1*/
+        /*Faça um programa para ler dois valores inteiros, 
+        e depois mostrar na tela a soma desses números com uma mensagem explicativa,
+        conforme exemplos.*/
+        Scanner entrada = new Scanner(System.in);
+
+//        System.out.println("Insira um primeiro valor");
+//        int primeiroValor;
+//        primeiroValor = entrada.nextInt();
+//
+//        System.out.println("Insira um segundo valor");
+//        int segundoValor;
+//        segundoValor = entrada.nextInt();
+//
+//        System.out.println("Os valores somados são:" + (primeiroValor + segundoValor));
+
+        /*exercicio 2*/
+        /*Faça um programa para ler o valor do raio de um círculo,
+        e depois mostrar o valor da área deste círculo com quatro
+        casas decimais conforme exemplos. Fórmula da área: area = π . raio2
+        Considere o valor de π = 3.14159*/
+
+//        System.out.println("Digite o raio do circulo");
+//        double raio;
+//        raio = entrada.nextDouble();
+//
+//        double pi = 3.14159;
+//
+//        System.out.println("O raio do circulo é:" + (Math.pow(raio,2) * pi));
+//        System.out.printf("O raio do circulo é %.4f:", (Math.pow(raio,2) * pi));
+
+        /*exercicio 3*/
+        /*Fazer um programa para ler quatro valores inteiros A, B, C e D.
+        A seguir, calcule e mostre a diferença do produto de A e B pelo produto de C e D segundo a fórmula:
+        DIFERENCA = (A * B - C * D).*/
+
+//        System.out.println("Insira o primeiro valor");
+//        double primeiroValor;
+//        primeiroValor = entrada.nextDouble();
+//
+//        System.out.println("Insira o segundo valor");
+//        double segundoValor;
+//        segundoValor = entrada.nextDouble();
+//
+//        System.out.println("Insira o terceiro valor");
+//        double terceiroValor;
+//        terceiroValor = entrada.nextDouble();
+//
+//        System.out.println("Insira o quarto valor");
+//        double quartoValor;
+//        quartoValor = entrada.nextDouble();
+//
+//        double diferenca = (primeiroValor * segundoValor)-(terceiroValor * quartoValor);
+//
+//        System.out.printf("Diferença é de: %.0f", diferenca);
+
+        /*exercicio 4*/
+        /*Fazer um programa que leia o número de um funcionário, seu número de horas trabalhadas,
+        o valor que recebe por hora e calcula o salário desse funcionário.
+        A seguir, mostre o número e o salário do funcionário, com duas casas decimais*/
+
+//        System.out.println("Insira o numero do funcionario");
+//        int numeroFuncionario;
+//        numeroFuncionario = entrada.nextInt();
+//
+//        System.out.println("Insira o numero de horas trabalhada");
+//        double horasTrabalhada;
+//        horasTrabalhada = entrada.nextDouble();
+//
+//        System.out.println("Insira o valor que recebe por horas");
+//        double valorHora;
+//        valorHora = entrada.nextDouble();
+//
+//        System.out.println("O numero do funcionario é:" + numeroFuncionario + ". O salario dele é: " + (horasTrabalhada * valorHora));
+//        System.out.printf("O numero do funcionario é:%d. O salario dele é:%.2f ", numeroFuncionario, (horasTrabalhada * valorHora));
+
+        /*exercicio 5*/
+        /*Fazer um programa para ler o código de uma peça 1,o número de peças 1, o valor unitário de cada peça 1,
+        o código de uma peça 2, o número de peças 2 e o valor unitário de cada peça 2.
+        Calcule e mostre o valor a ser pago.*/
+
+//        System.out.println("insira o codigo da peça 1");
+//        int codigoPeca1;
+//        codigoPeca1 = entrada.nextInt();
+//
+//        System.out.println("Insira o numero de peças 1");
+//        int quantidadePeca1;
+//        quantidadePeca1 = entrada.nextInt();
+//
+//        System.out.println("Insira o valor unitario de cada peça 2");
+//        double valorUnitarioPeca1;
+//        valorUnitarioPeca1 = entrada.nextDouble();
+//
+//        System.out.println("insira o codigo da peça 2");
+//        int codigoPeca2;
+//        codigoPeca2 = entrada.nextInt();
+//
+//        System.out.println("Insira o numero de peças 2");
+//        int quantidadePeca2;
+//        quantidadePeca2 = entrada.nextInt();
+//
+//        System.out.println("Insira o valor unitario de cada peça 2");
+//        double valorUnitarioPeca2;
+//        valorUnitarioPeca2 = entrada.nextDouble();
+//
+//        double valorTotalPeca1 = (quantidadePeca1 * valorUnitarioPeca1);
+//        double valorTotalPeca2 = (quantidadePeca2 * valorUnitarioPeca2);
+//        double valorTotal = valorTotalPeca1 + valorTotalPeca2;
+//
+//        System.out.printf("Valor Total: %.2f", valorTotal);
+
+        /*Exercício 06*/
+        /*Fazer um programa que leia três valores com ponto flutuante de dupla precisão: A, B e C. Em seguida, calcule e
+            mostre:
+        a) a área do triângulo retângulo que tem A por base e C por altura.
+        b) a área do círculo de raio C. (pi = 3.14159)
+        c) a área do trapézio que tem A e B por bases e C por altura.
+        d) a área do quadrado que tem lado B.
+        e) a área do retângulo que tem lados A e B.*/
+
+//        System.out.println("Insira os valores do triangulo de A,B,C respectivamente");
+//        double pi = 3.14159;
+//        float A;
+//        A = entrada.nextFloat();
+//        float B;
+//        B = entrada.nextFloat();
+//        float C;
+//        C = entrada.nextFloat();
+//
+//        /*A*/
+//        System.out.println("Triangulo = " + (A * C) / 2);
+//
+//        /*B*/
+//        System.out.println("Circulo = " + (Math.pow(C,2) * pi));
+//
+//        /*C*/
+//        float trapezio = (A + B) / 2 * C;
+//        System.out.println("Trapezio = " + trapezio);
+//
+//        /*D*/
+//        System.out.println("Quadrado = " + B*B);
+//
+//        /*E*/
+//        System.out.println("Retangulo = " + A*B);
+
+    }
+}
